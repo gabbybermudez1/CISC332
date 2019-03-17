32183cad1bf1f26309a2ac678b7ad797fcb9cba2
Gabby <gabbybermudez1@gmail.com>
2019-03-16 23:43:23 -0400

    added most error checking and edit pencil icon

--- events.php
+++ events.php
@@ -50,7 +50,7 @@
                 foreach($data as $row){
                     echo "<tr>";
                     echo "<td>". $row['speaker_first']. " " . $row['speaker_last'] . "</td>";
-                    echo "<td> <span id='event$eventID'>" . $row['session']."</span> <button onClick=editHandler($eventID)> Edit Event</button> </td>";
+                    echo "<td> <span id='event$eventID'>" . $row['session']."</span> <a onclick='editHandler($eventID)'><img src='./assets/edit_icon.png' style='height:10%;width:10%;margin-left:10px;'/> </a></td>";
                     echo "<td> " . $row['start_t'] ."</td>";
                     echo "<td> " . $row['end_t'] ."</td>";
                     echo "</tr>";
@@ -58,7 +58,7 @@
                 };
             }
             catch(PDOException $e){
-                    echo "Connection failed: " . $e->getMessage();
+                echo "Connection failed: " . $e->getMessage();
             }
             // end connection
             $db = null;
@@ -74,17 +74,19 @@
         <div class="modal-content">
         <span class="close">&times;</span>
             <div class="center-form">
-                <form class="modal-form"> 
+                <form class="modal-form" method="POST"> 
                     <h3> Edit Event</h3>
+                    <label> Speaker Name (First and Last) </label>
+                    <input type="text" name="name-input"  />
                     <label> Session </label>
-                    <input type="text" id="sessionEditor" />
+                    <input type="text" id="session-editor" name="session-input" />
                     <label> Day </label>
-                    <input type="text"  />
+                    <input type="text" name="day-input" />
                     <label> Start Time </label>
-                    <input type="text"  />
+                    <input type="text" name="start-time-input"  />
                     <label> End Time </label>
-                    <input type="text"  />
-                    <input type=submit />
+                    <input type="text"  name="end-time-input"/>
+                    <input type=submit name="event-changes"/>
                 </form>
             </div>
         </div>
@@ -93,10 +95,54 @@
 </body>
 </html>
 
+<!-- PHP code for the form inside the modal! -->
+<?php 
+
+// helper function that allows us to split the name of a user
+function splitName($fullName){
+    $nameArray = preg_split("~\s~",$fullName);
+    return $nameArray;
+}
+
+if(isset($_POST["event-changes"])){
+    $servername = "localhost";
+    $databaseName = "conference_db";
+    $username = "root";
+
+    //variables named the same way they are in the database
+    $session = $_POST['session-input'];
+    $session_day = "Day ". $_POST['day-input'];
+    $start_t =date("h:i:s" , strtotime($_POST['start-time-input']));
+    $end_t = date("h:i:s" , strtotime($_POST['end-time-input']));
+    $speaker_first = splitName($_POST['name-input'])[0];
+    $speaker_last = splitName($_POST['name-input'])[1];
+  
+    // code containing error handling 
+    if( ($session_day != "Day 1" and $session_day != "Day 2" ) or (!isset($speaker_last) ) ) {
+        echo "You've entered invalid information. Nothing was updated in the database. Please try again";
+    
+    }
+
+    $editSql = "UPDATE sessions SET  session='$session', session_day='$session_day', start_t=$start_t, end_t=$end_t, speaker_first='$speaker_first',speaker_last='$speaker_last'  WHERE session='$session'";
+    
+    echo $editSql;
+
+    // try{
+    //     $db = new PDO("mysql:host=$servername;dbname=$databaseName", $username);
+    //     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // }
+    // catch(PDOException $e){
+    //     echo "Connection failed: " . $e->getMessage();
+    // }
+    // $db = null;
+}
+
+?>
+
 
 <script>
 var modal = document.getElementById('myModal');
-var sessionEditor = document.querySelector('#sessionEditor');
+var sessionEditor = document.querySelector('#session-editor');
 var closeModal = document.getElementsByClassName("close")[0]; 
 
 
@@ -112,3 +158,13 @@ closeModal.onclick = function() {
 
 </script> 
 
+
+<!-- 
+UPDATE sessions 
+SET session='OS Systems', session_day='Day 1',speaker_first='Gabby', speaker_last='Bermudez' WHERE session='OS Systems'
+
+
+UPDATE sessions 
+SET session='OS Systems', session_day='Day 1', 
+speaker_first='Gabby', speaker_last='Bee' 
+WHERE session='OS Systems' -->
